d0f5771b5610118325ed77b911619d0802f0974c
Bazán Lozano <jesus.bazan@entel.pe>
2025-07-29 03:54:59 -0500

    correccion main.py

--- app/main.py
+++ app/main.py
@@ -184,9 +184,10 @@ except Exception:
 
 if __name__ == "__main__":
     uvicorn.run(
-        "app.main:app",
-        host="0.0.0.0",
-        port=8000,
-        reload=True,
+        "main:app",
+        host=settings.HOST,
+        port=settings.PORT,
+        reload=settings.DEBUG,
         log_level="info"
     )
+    
